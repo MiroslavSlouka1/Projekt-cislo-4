ce793a44de029b70b659684d8f55112131e5d90d
MiroslavSlouka1 <163758455+MiroslavSlouka1@users.noreply.github.com>
2025-01-03 19:44:22 +0100

    Update Projekt c. 4.sql

--- Projekt c. 4.sql
+++ Projekt c. 4.sql
@@ -1,79 +1,73 @@
--- krok 1	
-CREATE OR REPLACE TABLE t_Miroslav_Slouka_project_SQL_primary_final AS
-	SELECT payroll_year, payroll_quarter, 
-	    CONCAT(payroll_year, '-', payroll_quarter) AS payroll_year_quarter,
-	    value,
-		cpvt.name AS cpvt_name,
-		cpu.name AS cpu_name,
-		cpc.name AS cpc_name,
-		cpib.name AS cpib_name
-	FROM czechia_payroll cp 
-	LEFT JOIN czechia_payroll_value_type cpvt 
-	    ON cp.value_type_code = cpvt.code 
-	LEFT JOIN czechia_payroll_unit cpu 
-	    ON cp.unit_code = cpu.code 
-	LEFT JOIN czechia_payroll_calculation cpc 
-		ON cp.calculation_code = cpc.code 
-	LEFT JOIN czechia_payroll_industry_branch cpib 
-		ON cp.industry_branch_code = cpib.code; 	
-	
--- krok 2   
-CREATE TABLE question2 AS
+-- krok 1 --
+
+CREATE OR REPLACE TABLE t_Miroslav_Slouka_project_SQL_primary_final AS 
 	SELECT 
-	    payroll_year, 
-	    payroll_quarter, 
-	    payroll_year_quarter,
-	    value,
-	    cpvt_name,
-	    cpu_name,
-	    cpc_name,
-	    cpib_name,
-	    NULL AS pcpc_name,
-	    NULL AS pcp_unit
+	    payroll_year AS year,
+	    AVG(value) AS avg_payroll,
+	    cpvt.name AS payroll_category,
+	    cpu.name AS payroll_unit,
+	    cpib.name AS industry_branch,
+	    NULL AS avg_price,
+	    -- NULL AS category_code,
+	    NULL AS food_name,
+	    NULL AS quantity,
+	    NULL AS unit
 	FROM 
-	    t_Miroslav_Slouka_project_SQL_primary_final
+	    czechia_payroll cp 
+	LEFT JOIN 
+	    czechia_payroll_value_type cpvt 
+	    ON cp.value_type_code = cpvt.code 
+	LEFT JOIN 
+	    czechia_payroll_unit cpu 
+	    ON cp.unit_code = cpu.code 
+	LEFT JOIN 
+	    czechia_payroll_calculation cpc 
+	    ON cp.calculation_code = cpc.code 
+	LEFT JOIN 
+	    czechia_payroll_industry_branch cpib 
+	    ON cp.industry_branch_code = cpib.code
+	WHERE 
+	    cpvt.name = 'Průměrná hrubá mzda na zaměstnance' 
+	    AND cpc.name = 'fyzický'
+	    AND cpib.name IS NOT NULL 
+	GROUP BY  
+	    payroll_year,
+	    cpvt.name,
+	    cpu.name,
+	    cpib.name
 	UNION ALL
-		SELECT 
-		-- NULL AS payroll_year,
-		-- NULL AS payroll_quarter,
-		YEAR(date_from) AS payroll_year,
-		QUARTER(date_from) AS payroll_quarter,
-		CONCAT(YEAR(date_from), '-', QUARTER(date_from)) AS payroll_year_quarter,  
-		AVG(value) AS value,      
-		cpc1.name AS cpvt_name,
-		'Price' AS cpu_name,  
-		NULL AS cpc_name,
-		NULL AS cpib_name,    
-		cpc1.price_value AS pcpc_value,
-		cpc1.price_unit AS pcpc_unit
-		FROM 
-		    czechia_price cp 
-		LEFT JOIN 
-		    czechia_price_category cpc1  
-		    ON cp.category_code = cpc1.code
-		WHERE 
-		    region_code IS NULL
-		GROUP BY
-	    	-- YEAR(date_from) AS payroll_year,
-	    	-- QUARTER(date_from) AS payroll_quarter,
-		    CONCAT(YEAR(date_from), '-', QUARTER(date_from)), 
-		    cpc1.name;
-		    -- cpc1.price_value, 
-		    -- cpc1.price_unit;
+	SELECT 
+	    YEAR(date_from) AS year,
+	    NULL AS avg_payroll,  -- Přidáno pro spojení s prvním dotazem
+	    NULL AS payroll_category,
+	    NULL AS payroll_unit,
+	    NULL AS industry_branch, -- Přidáno pro spojení s prvním dotazem
+	    AVG(value) AS avg_price,
+	    -- cp.category_code,
+	    cpc1.name AS food_name,
+	    cpc1.price_value AS quantity,
+	    cpc1.price_unit AS unit
+	FROM 
+	    czechia_price cp 
+	LEFT JOIN 
+	    czechia_price_category cpc1  
+	    ON cp.category_code = cpc1.code
+	WHERE 
+	    region_code IS NULL
+	GROUP BY
+	    `year`, 
+	    cp.category_code,
+	    cpc1.name,
+	    cpc1.price_value,
+	    cpc1.price_unit
+	ORDER BY
+	    `year`,
+	    industry_branch, 
+	    food_name;
+
+-- krok 2 --	   
 	   
--- krok 3	    
-CREATE OR REPLACE TABLE 
-	t_Miroslav_Slouka_project_SQL_primary_final 
-AS SELECT 
-	* 
-FROM question2;
-
--- krok 4
-DROP TABLE question2;
-
-
--- krok 5
-CREATE TABLE t_miroslav_slouka_project_SQL_secondary_final AS
+CREATE OR REPLACE TABLE t_miroslav_slouka_project_SQL_secondary_final AS
 	SELECT 
 		year,
 		country,
@@ -86,256 +80,212 @@ CREATE TABLE t_miroslav_slouka_project_SQL_secondary_final AS
 			FROM countries c 
 			WHERE continent = 'Europe'
 	);
-
-
--- otazka cislo 1 -----------------------
+	   
+-- otázka č.1 --
+	   	   
 WITH question1 AS (
     SELECT 
-        payroll_year_quarter, 
-        value, 
-        LAG(payroll_year_quarter, 4) OVER (PARTITION BY cpib_name ORDER BY payroll_year_quarter, payroll_quarter) AS previous_year,  
-        LAG(value, 4) OVER (PARTITION BY cpib_name ORDER BY payroll_year, payroll_quarter) AS previous_value, 
-        cpib_name   
+        year, 
+        avg_payroll , 
+        LAG(year) OVER (PARTITION BY industry_branch ORDER BY year) AS previous_year,  
+        LAG(avg_payroll) OVER (PARTITION BY industry_branch ORDER BY avg_payroll) AS previous_avg_payroll, 
+        industry_branch  
     FROM 
         t_miroslav_slouka_project_sql_primary_final tmsp 
-    WHERE 
-        cpvt_name = 'Průměrná hrubá mzda na zaměstnance' 
-        AND cpc_name = 'fyzický'
+    WHERE 1=1
+        AND industry_branch IS NOT NULL
 )
 SELECT 
-    payroll_year_quarter, 
-    value, 
-    previous_year,  
-    previous_value, 
-    cpib_name,
-    value - previous_value AS difference, 
+    year, 
+    avg_payroll, 
+    previous_year, 
+    previous_avg_payroll,
+    industry_branch,
+    avg_payroll - previous_avg_payroll AS difference_payroll, 
     CASE
-    	WHEN value - previous_value > 0 THEN 'Mzda roste'
-    	WHEN value - previous_value < 0 THEN 'Mzda klesá'
-    	WHEN value - previous_value = 0 THEN 'Mzda stagnuje'
+    	WHEN avg_payroll - previous_year > 0 THEN 'Mzda roste'
+    	WHEN avg_payroll - previous_year < 0 THEN 'Mzda klesá'
+    	WHEN avg_payroll - previous_year = 0 THEN 'Mzda stagnuje'
     END AS result
 FROM 
     question1
 WHERE 1=1
     AND previous_year IS NOT NULL
-    AND previous_value IS NOT NULL
-    AND cpib_name IS NOT NULL
-ORDER BY 
-	payroll_year_quarter
-	-- difference;
+ORDER BY difference_payroll;
 
-	
--- otazka cislo 2 ----------  
- WITH min_max_payroll AS (
+       
+-- otázka č.2 --
+ 
+WITH min_max_year_price AS (
     SELECT 
-        MIN(payroll_year_quarter) AS min_payroll,
-        MAX(payroll_year_quarter) AS max_payroll
+        MIN(year) AS min_year_price,
+        MAX(year) AS max_year_price
     FROM t_miroslav_slouka_project_sql_primary_final
-    WHERE cpu_name = 'Price'
-),
-query1 AS (
-    SELECT 
-        payroll_year_quarter,
-        value,
-        cpib_name
-    FROM t_miroslav_slouka_project_sql_primary_final tmspspf 
-    WHERE 
-        cpvt_name = 'Průměrná hrubá mzda na zaměstnance' 
-        AND cpc_name = 'fyzický'
-        AND payroll_year_quarter IN (SELECT min_payroll FROM min_max_payroll UNION SELECT max_payroll FROM min_max_payroll)
-),
-query2 AS (
-    SELECT 
-       *
-    FROM t_miroslav_slouka_project_sql_primary_final tmspspf 
-    WHERE 
-        (cpvt_name LIKE '%mléko%' OR cpvt_name LIKE '%chléb%')
-        AND payroll_year_quarter IN (SELECT min_payroll FROM min_max_payroll UNION SELECT max_payroll FROM min_max_payroll)
+    WHERE avg_price IS NOT NULL 
+), query1 AS (
+	SELECT
+		tmsp.`year`,
+	    tmsp.food_name,
+	    tmsp.avg_price
+	FROM t_miroslav_slouka_project_sql_primary_final tmsp
+	JOIN min_max_year_price mmyp
+		ON tmsp.`year` = mmyp.min_year_price 
+		OR tmsp.`year` = mmyp.max_year_price
+	WHERE 
+		(tmsp.food_name LIKE '%mléko%' 
+		OR tmsp.food_name LIKE '%chléb%')
+	GROUP BY 
+		tmsp.`year`, 
+	  	tmsp.food_name,
+	  	tmsp.avg_price
+), query2 AS (
+	SELECT
+    	tmsp.`year`,
+	    AVG(tmsp.avg_payroll) AS all_avg_payroll
+	FROM t_miroslav_slouka_project_sql_primary_final tmsp
+	JOIN min_max_year_price mmyp
+		ON tmsp.`year` = mmyp.min_year_price 
+		OR tmsp.`year` = mmyp.max_year_price
+	WHERE 
+		tmsp.avg_payroll IS NOT NULL 
+	GROUP BY 
+	    tmsp.`year`, 
+	    tmsp.food_name,
+	    tmsp.avg_price
 )
 SELECT 
-    q1.payroll_year_quarter,
-    q1.value AS value,
-    q1.cpib_name AS name,
-    q2.value AS price,
-    q2.cpvt_name,
-    TRUNCATE(q1.value / q2.value, 0) AS quantity
+	q1.*,
+    q2.all_avg_payroll,
+    TRUNCATE(q2.all_avg_payroll / q1.avg_price, 0) AS quantity
+FROM
+	query1 q1
+LEFT JOIN query2 q2
+    ON q1.`year` = q2.`year` 
+
+    
+    
+-- otázka č.3 --
+	
+WITH query1 AS (
+    SELECT
+        `year`, 
+        food_name,
+        avg_price,
+        LAG(year) OVER (PARTITION BY food_name ORDER BY `year`) AS previous_year,  
+        LAG(avg_price) OVER (PARTITION BY food_name ORDER BY `year`) AS previous_avg_price 
+    FROM t_miroslav_slouka_project_sql_primary_final
+    WHERE
+        food_name IS NOT NULL
+)
+SELECT 
+	*,
+	avg_price - previous_avg_price AS difference,
+	ROUND(((avg_price - previous_avg_price) / previous_avg_price) * 100, 2) AS price_increase_percentage
+FROM query1
+WHERE
+	previous_year IS NOT NULL
+ORDER BY 
+    price_increase_percentage;
+	-- food_name,
+	-- `year`;
+	
+-- otázka č.4 --
+
+WITH query1 AS (
+    SELECT
+        `year`, 
+   	    AVG(avg_price)AS all_avg_price,   --  OVER (PARTITION BY `year`), 
+   	    LAG(`year`) OVER (ORDER BY `year`) AS previous_year,   	    
+   	    LAG(AVG(avg_price)) OVER (ORDER BY `year`) AS previous_avg_price
+    FROM t_miroslav_slouka_project_sql_primary_final
+    WHERE
+        food_name IS NOT NULL
+    GROUP BY
+    	`year`
+	    ), query2 AS (
+    SELECT
+         `year`,     
+         AVG(avg_payroll)AS all_avg_payroll,
+   	     LAG(`year`) OVER (ORDER BY `year`) AS previous_year,
+   	     LAG(AVG(avg_payroll)) OVER (ORDER BY `year`) AS previous_avg_payroll
+    FROM t_miroslav_slouka_project_sql_primary_final
+    WHERE
+        avg_payroll IS NOT NULL
+    GROUP BY
+    	`year`
+)
+SELECT 
+	-- q1.*,
+    q1.`year`,
+    ROUND(((q1.all_avg_price - q1.previous_avg_price) / q1.previous_avg_price) * 100, 2) AS price_value_percentage,
+    -- q2.*,
+    ROUND(((q2.all_avg_payroll - q2.previous_avg_payroll) / q2.previous_avg_payroll) * 100, 2) AS payroll_value_percentage,
+    (ROUND(((q1.all_avg_price - q1.previous_avg_price) / q1.previous_avg_price) * 100, 2)) -
+    (ROUND(((q2.all_avg_payroll - q2.previous_avg_payroll) / q2.previous_avg_payroll) * 100, 2)) AS difference_value
 FROM 
-    query1 q1
+	query1 q1
 LEFT JOIN 
     query2 q2
 ON 
-    q1.payroll_year_quarter = q2.payroll_year_quarter
+	q1.`year` = q2.`year`
 WHERE 
-    q1.cpib_name IS NOT NULL
-ORDER BY
-    q2.cpvt_name, 
-    q1.payroll_year_quarter;
-   
-   
-   
--- otazka cislo 3 ----------  
-WITH question1 AS (
-    SELECT 
-        payroll_year, 
-        LAG(payroll_year) OVER (PARTITION BY cpvt_name ORDER BY payroll_year) AS previous_year, 
-        AVG(value) AS average_price,
-        LAG(AVG(value)) OVER (PARTITION BY cpvt_name ORDER BY payroll_year) AS previous_price, 
-        cpvt_name
-    FROM 
-        t_miroslav_slouka_project_sql_primary_final tmsp 
-    WHERE
-        cpu_name = 'Price'
-    GROUP BY 
-        payroll_year,
-        cpvt_name
-    ORDER BY 
-        cpvt_name,
-        payroll_year
-),
-price_changes AS (
-    SELECT
-        *,
-        average_price - previous_price AS difference,
-        ROUND(((average_price - previous_price) / previous_price) * 100, 2) AS price_increase_percentage
-    FROM question1
-    WHERE previous_year IS NOT NULL
-)
-SELECT 
-    *
-FROM 
-    price_changes
-WHERE 
-    price_increase_percentage = (
-        SELECT MIN(price_increase_percentage)
-        FROM price_changes
-    )
-ORDER BY 
-    cpvt_name, 
-    payroll_year;
-   
-
--- otazka cislo 4 ----------
-WITH query1 AS (
-SELECT 
-    payroll_year, 
-    LAG(payroll_year) OVER (PARTITION BY cpvt_name ORDER BY payroll_year) AS previous_year, 
-    AVG(value) AS avg_value,  
-    LAG(AVG(value)) OVER (PARTITION BY cpvt_name ORDER BY payroll_year) AS avg_previous_price,
-    cpvt_name
-FROM 
-    t_miroslav_slouka_project_sql_primary_final tmsp 
-WHERE
-    cpu_name = 'Price'
-GROUP BY 
-    payroll_year,
-    cpvt_name
-), query2 AS (
-    SELECT 
-        payroll_year, 
-        value, 
-        LAG(payroll_year) OVER (PARTITION BY cpib_name ORDER BY payroll_year) AS previous_year,
-        AVG(value) AS p_avg_value,       
-        LAG(AVG(value)) OVER (PARTITION BY cpib_name ORDER BY payroll_year) AS p_avg_previous_value, 
-        cpib_name   
-    FROM 
-        t_miroslav_slouka_project_sql_primary_final tmsp 
-    WHERE 
-        cpvt_name = 'Průměrná hrubá mzda na zaměstnance' 
-        AND cpc_name = 'fyzický'
-   GROUP BY 
-        payroll_year,
-        cpvt_name     
-)
-SELECT 
-    q1.payroll_year, 
-    q1.cpvt_name,
-    -- q1.*,
-	-- q1.avg_value - q1.avg_previous_price AS payroll_rozdil,
-	ROUND(((q1.avg_value - q1.avg_previous_price) / q1.avg_previous_price) * 100, 2) AS price_value_percentage,
-	q2.cpib_name,
-    ROUND(((q2.p_avg_value - q2.p_avg_previous_value) / q2.p_avg_previous_value) * 100, 2) AS payroll_value_percentage
-FROM 
-	query2 q2
-LEFT JOIN 
-    query1 q1
-ON 
-  q2.payroll_year = q1.payroll_year
-WHERE 1=1
-	AND avg_previous_price IS NOT NULL
-    AND cpib_name IS NOT NULL
-    AND q1.previous_year IS NOT NULL
+    q1.previous_year IS NOT NULL
     AND q2.previous_year IS NOT NULL 
-ORDER BY price_value_percentage DESC
--- ORDER BY payroll_value_percentage DESC
+ORDER BY difference_value DESC
 
+   
 
-	
--- otazka cislo 5 ----------
+ -- otázka č.5 --  
+
 WITH query1 AS (
-SELECT 
-    payroll_year, 
-    LAG(payroll_year) OVER (PARTITION BY cpvt_name ORDER BY payroll_year) AS previous_year, 
-    AVG(value) AS avg_value,  
-    LAG(AVG(value)) OVER (PARTITION BY cpvt_name ORDER BY payroll_year) AS avg_previous_price,
-    cpvt_name
-FROM 
-    t_miroslav_slouka_project_sql_primary_final tmsp 
-WHERE
-    cpu_name = 'Price'
-GROUP BY 
-    payroll_year,
-    cpvt_name
+    SELECT
+        `year`, 
+   	    -- AVG(avg_price) AS all_avg_price, 
+   	    -- LAG(`year`) OVER (ORDER BY `year`) AS previous_year,   	    
+   	    -- LAG(AVG(avg_price)) OVER (ORDER BY `year`) AS previous_avg_price,
+   	    AVG(avg_price) - LAG(AVG(avg_price)) OVER (ORDER BY `year`) AS difference_price
+    FROM t_miroslav_slouka_project_sql_primary_final
+    WHERE
+        food_name IS NOT NULL
+    GROUP BY
+    	`year`
 ), query2 AS (
-    SELECT 
-        payroll_year, 
-        value, 
-        LAG(payroll_year) OVER (PARTITION BY cpib_name ORDER BY payroll_year) AS previous_year,
-        AVG(value) AS p_avg_value,       
-        LAG(AVG(value)) OVER (PARTITION BY cpib_name ORDER BY payroll_year) AS p_avg_previous_value, 
-        cpib_name   
-    FROM 
-        t_miroslav_slouka_project_sql_primary_final tmsp 
-    WHERE 
-        cpvt_name = 'Průměrná hrubá mzda na zaměstnance' 
-        AND cpc_name = 'fyzický'
-   GROUP BY 
-        payroll_year,
-        -- cpvt_name
-        cpib_name     
- ), query3 AS (
- 	SELECT
- 	    YEAR,
- 	    LAG(GDP) OVER (ORDER BY year) AS previous_GDP,
- 		GDP
+    SELECT
+        `year`, 
+   	     -- AVG(avg_payroll) AS all_avg_payroll, 
+   	     -- LAG(`year`) OVER (ORDER BY `year`) AS previous_year,   	    
+   	     -- LAG(AVG(avg_payroll)) OVER (ORDER BY `year`) AS previous_avg_payroll,
+   	     AVG(avg_payroll) - LAG(AVG(avg_payroll)) OVER (ORDER BY `year`) AS difference_payroll
+    FROM t_miroslav_slouka_project_sql_primary_final
+     WHERE
+        avg_payroll IS NOT NULL
+    GROUP BY
+    	`year` 
+), query3 AS (    
+     	SELECT
+ 	    `year`,
+ 	    -- GDP, 
+ 	    -- LAG(GDP) OVER (ORDER BY year) AS previous_GDP,
+ 		GDP - LAG(GDP) OVER (ORDER BY year) AS difference_GDP
  	FROM
 		t_miroslav_slouka_project_sql_secondary_final tmss
     WHERE
     	country = 'Czech Republic'
-) 
-SELECT 
-    q1.payroll_year, 
-    q1.cpvt_name,
-    -- q1.*,
-	q2.cpib_name,    
-	q1.avg_value - q1.avg_previous_price AS price_difference,
-	q2.p_avg_value - q2.p_avg_previous_value AS payroll_difference,
-	q3.GDP - q3.previous_GDP AS GDP_difference
+    	AND GDP IS NOT NULL
+)    	
+SELECT
+    q1.`year`,
+	difference_price,
+	difference_payroll,
+	difference_GDP
 FROM 
-	query2 q2
+	query1 q1
 LEFT JOIN 
-    query1 q1
-ON 
-  q2.payroll_year = q1.payroll_year
+    query2 q2
+	ON q1.year = q2.year
 LEFT JOIN
     query3 q3
-ON
-  q2.payroll_year =q3. year 
-WHERE 1=1
-	AND avg_previous_price IS NOT NULL
-    AND cpib_name IS NOT NULL
-    AND q1.previous_year IS NOT NULL
-    AND q2.previous_year IS NOT NULL 
-ORDER BY payroll_year;
--- ORDER BY price_difference
-
-
+	ON q1.year =q3.year 
+WHERE 
+	difference_price IS NOT NULL 
+    
